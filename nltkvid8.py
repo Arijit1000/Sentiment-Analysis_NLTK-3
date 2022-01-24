4388e375c8134e8956761c9cd775db70e564cd5e
Arijit1000 <73710898+Arijit1000@users.noreply.github.com>
2022-01-25 02:15:48 +0530

    Update nltkvid8.py

--- nltkvid8.py
+++ nltkvid8.py
@@ -1,4 +1,10 @@
 
+
+# Lemmatization is the process of grouping together the different inflected forms of a word so they can be analyzed as a single item. Lemmatization is similar to stemming but it brings context to the words. So it links words with similar meanings to one word. 
+Text preprocessing includes both Stemming as well as Lemmatization
+
+
+
 from nltk.stem import WordNetLemmatizer
 
 lemmatizer = WordNetLemmatizer()
