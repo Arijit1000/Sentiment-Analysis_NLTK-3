ca0e8999550ae5ab9c14189056a455075226df1f
Arijit1000 <73710898+Arijit1000@users.noreply.github.com>
2022-01-25 02:16:38 +0530

    Update nltkvid8.py

--- nltkvid8.py
+++ nltkvid8.py
@@ -1,7 +1,7 @@
 
 
 # Lemmatization is the process of grouping together the different inflected forms of a word so they can be analyzed as a single item. Lemmatization is similar to stemming but it brings context to the words. So it links words with similar meanings to one word. 
-Text preprocessing includes both Stemming as well as Lemmatization
+# Text preprocessing includes both Stemming as well as Lemmatization
 
 
 
